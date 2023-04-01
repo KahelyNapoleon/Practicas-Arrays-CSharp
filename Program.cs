13dbf0d141fdc8c24bc05877b7220d19422950e6
Kahel <aguirregabrielnahuel@hotmail.com>
2023-03-31 21:42:32 -0300

    algoritmo por arreglar

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-           int[] array2 = {2,3,4,4,4,5,1,2,2};
+            //Que numero se repite mas veces en este Array?
+            //Imprimirlo por consola
+
+           int[] array2 = {1,3,4,4,4,4,4,5,1,1,1};
+
+           int bestStart = 0;
+           int bestLen = 0;
+
+           for(int i = 1; i < array2.Length; i++)
+           {
+             if(array2[i] != array2[i - 1])
+             {
+                return;
+             }else{
+                bestStart = array2[i];
+                bestLen ++;
+             }
+           }
+
+           for(int j = 0; j < bestLen; j++)
+           {
+            Console.Write(bestStart);
+           }
+
            
            
            
