4e2c0ab72199af0cf6eedfefb1e1d8e4148a1ccd
Aguirre Gabriel <AguirreGabrielNahuel@hotmail.com>
2024-04-12 10:26:47 -0300

    Creacion del proyecto de ejercicios con matrices

---
+++ Ejercicios-Con-Matrices/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicios_Con_Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
